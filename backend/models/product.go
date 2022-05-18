2c028bc4dc33e7fcaeda0539012707d5c036699e
watariRyo <d.silv.vc1919@gmail.com>
2022-05-18 09:33:15 +0900

    ページネート共通化

--- backend/models/product.go
+++ backend/models/product.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Product struct {
 	Id uint `json:"id"`
-	Title string `json:"Title"`
+	Title string `json:"title"`
 	Description string `json:"description"`
 	Image string `json:"image"`
 	Price float64 `json:"price"`
